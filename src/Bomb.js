cbfc32cb155336ce974cf965277593cfa87a31f8
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-05-26 12:09:27 -0400

    Completed assignment

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
 // your Bomb code here!
+
+//decreate in time
+//explode at 0
+
+
+import React, { Component } from 'react';
+
+class Bomb extends Component {
+    constructor(props){
+        super()
+        this.state = { //setting the initial state of the bomb app
+            secondsLeft: props.initialCount,
+        }
+    }
+    
+    
+    render() {
+
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+        return <div>{message}</div> 
+    }
+}
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,20 @@
 // your ImageSlider code here!
+
+import React, { Component } from 'react';
+
+class ImageSlider extends Component {
+    constructor(){ //initial value of the state set in this constructor
+        super(); //super allows us to inherit form the paretn class React allowing us access to methods like this and variables from parents class
+        this.state = {
+            currentSlideIndex: 0
+        }
+
+    }
+    render() {
+        return <h1>I am on slide {this.state.currentSlideIndex}</h1>
+    }
+
+
+}
+
+export default ImageSlider;
